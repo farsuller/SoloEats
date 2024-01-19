e6dfd604f6a87874c8c6e9794bdf04ad20cde282
farsuller <farsuller2020@gmail.com>
2024-01-19 16:16:03 +0800

    build version 4, added google services

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ import java.util.Properties
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("com.google.gms.google-services")
     id("dagger.hilt.android.plugin")
     kotlin("kapt")
 
